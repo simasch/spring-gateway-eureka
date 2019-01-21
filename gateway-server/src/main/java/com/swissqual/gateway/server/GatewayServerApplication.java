4a26cfe7a9d7126fdac463f3e79e8e5044f19031
Simon Martinelli <simon@72.services>
2019-01-21 11:41:28 +0100

    init

---
+++ gateway-server/src/main/java/com/swissqual/gateway/server/GatewayServerApplication.java
@@ -0,0 +1,16 @@
+package com.swissqual.gateway.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class GatewayServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayServerApplication.class, args);
+    }
+
+}
+
